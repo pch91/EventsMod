4c21eac0c496b836374fbe97100131abf79f974d
PCH91 <scorpionsk8er@hotmail.com>
2023-08-10 13:39:49 -0300

    Add project files.

---
+++ EventsMod.cs
@@ -0,0 +1,75 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EventsMod
+{
+    [BepInPlugin("EventsMod", "Events Mod", "0.0.0.1")]
+    public class EventsMod : BaseUnityPlugin
+    {
+        public static EventsMod Instance;
+
+        private static string loglevel = "INFO";
+
+        public enum Logs
+        {
+            DEBUG = 1,
+            ERROR = 2,
+            INFO = 0,
+        }
+
+        public static void Log(string line, Logs level)
+        {
+            //Debug.Log((int)Enum.Parse(typeof(Logs), loglevel));
+
+            if ((int)Enum.Parse(typeof(Logs), loglevel) - (int)level < 1)
+            {
+                Debug.Log("["+ level + "    :   EventsMod] " + line);
+            }
+        }
+
+
+        public static Dictionary<string, object> fconfig = new Dictionary<string, object>();
+        public static Dictionary<string, object> fconfigEvents = new Dictionary<string, object>();
+        private Dictionary<string, object> mainconfigs = new Dictionary<string, object>();
+        private Dictionary<string, object> configs = new Dictionary<string, object>();
+
+        private void Awake()
+        {
+            try
+            {
+                Log("Start - EventsMod", Logs.INFO);
+                Instance = this;
+                Harmony.DEBUG = true;
+                var harmony = new Harmony("net.pch91.stationeers.EventsMod.patch");
+                LoadConfigsOnDictionary();
+                Handleconfig();
+                harmony.PatchAll();
+                Log("Finish patch", Logs.INFO);
+            }
+            catch (Exception e)
+            {
+                Debug.LogException(e);
+            }
+        }
+
+        private void Handleconfig()
+        {
+            mainconfigs.Add("LogEnabled", Config.Bind("0 - General configuration", "Log Level", "info", "Enable or disable logs. values can be debug , info or error"));
+            fconfigEvents.Add("confcinc", Config.Bind("0 - Incidents", "Config custom incidents Values", "info", "Type number need have grater than 100 \n For add more than one only separate with # \n For each one need stay in format - type|MaxPerTile|SpawnChance|IsRepeating|MaxDelay|MinDelay|CanLaunchOutsideTile|Serialize|RequiresHumanInTile|RunOnTileEnter|ContainStructures"));
+            fconfigEvents.Add("incMetName", Config.Bind("0 - Incidents", "Incdents Method Name", "info", "for use one pre configured incidents method put there."));
+            fconfigEvents.Add("chanincparam", Config.Bind("0 - Incidents", "Change Incdents Method Param", "info", "need stay in this format nameMethod|Param|value \n For more than one use # for separeted"));
+
+
+            loglevel = (mainconfigs["LogEnabled"] as ConfigEntry<string>).Value.ToUpper();
+        }
+
+        private void LoadConfigsOnDictionary()
+        {
+
+        }
+    }
+}
---
+++ patch/IncidentsPatch.cs
@@ -0,0 +1,137 @@
+﻿using Assets.Scripts;
+using Assets.Scripts.Networking;
+using HarmonyLib;
+using JetBrains.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+using static WorldManager;
+
+namespace EventsMod.patch
+{
+    [HarmonyPatch(typeof(WorldManager))]
+    [HarmonyPatch("SetWorldEnvironments")]
+    public class SetWorldEnvironmentsPatch
+    {
+        static List<string> incidentsName = new List<string>();
+
+        public SetWorldEnvironmentsPatch()
+        {
+            incidentsName.Add("AddIncident");
+        }
+
+        [UsedImplicitly]
+        [HarmonyPrefix]
+        public static void prefixPatch(ref TerrainFeatureSettings __instance)
+        {
+            EventsMod.Log("SetWorldEnvironmentsPatch:: prefixPatch --> carregando Incidents", EventsMod.Logs.DEBUG);
+            SetWorldEnvironmentsPatch incidentsPatch = new SetWorldEnvironmentsPatch();
+            EventsMod.Log("SetWorldEnvironmentsPatch:: prefixPatch --> get incidentsName size " + incidentsName.Count(), EventsMod.Logs.DEBUG);
+            IEnumerable<TerrainFeatureIncident> lt = incidentsPatch.addIncidents(incidentsName);
+            EventsMod.Log("SetWorldEnvironmentsPatch:: prefixPatch --> carregando "+ lt.Count()+ " Incidents", EventsMod.Logs.INFO);
+            foreach (TerrainFeatureIncident incident in lt)
+            {
+                if (!TileSystem.Instance.CurrentIncidentTypes.ContainsKey(incident.Type))
+                {
+                    TileSystem.Instance.CurrentIncidentTypes.Add(incident.Type, incident);
+                    TileSystem.WorldContainsIncidents = true;
+                    EventsMod.Log("SetWorldEnvironmentsPatch:: TakePlantDrinkPatch --> adicionado Incident " + incident.Type, EventsMod.Logs.DEBUG);
+                }
+            }
+            EventsMod.Log("SetWorldEnvironmentsPatch:: prefixPatch --> carregado", EventsMod.Logs.INFO);
+
+            //__instance.IncidentFeatures.AddRange(incidentsPatch.addIncidents(incidentsName));
+        }
+
+        private IEnumerable<TerrainFeatureIncident> addIncidents(List<string> incidentsName)
+        {
+            EventsMod.Log("SetWorldEnvironmentsPatch:: addIncidents --> carregando Incidents", EventsMod.Logs.DEBUG);
+            List<TerrainFeatureIncident> response = new List<TerrainFeatureIncident>();
+            foreach (string incident in incidentsName)
+            {
+                EventsMod.Log("SetWorldEnvironmentsPatch:: addIncidents --> Incident "+ incident, EventsMod.Logs.DEBUG);
+                TerrainFeatureIncident terrainFeatureIncident = new TerrainFeatureIncident();
+
+                try
+                {
+                    response.Add((TerrainFeatureIncident)GetType().GetMethod(incident, BindingFlags.NonPublic | BindingFlags.Instance).Invoke(this, new object[] { terrainFeatureIncident }));
+                }catch (Exception ex)
+                {
+                    Debug.LogException(ex);
+                }
+                EventsMod.Log("SetWorldEnvironmentsPatch:: addIncidents --> adicionado " + incident, EventsMod.Logs.DEBUG);
+            }
+            return response;
+        }
+
+        // -------------------------------incidents-------------------------------------------------------
+
+        private TerrainFeatureIncident AddIncident(TerrainFeatureIncident terrainFeatureIncident)
+        {
+            EventsMod.Log("SetWorldEnvironmentsPatch:: AddIncident --> carregou Incidents " + 44, EventsMod.Logs.INFO);
+            terrainFeatureIncident.Type = 44;
+            terrainFeatureIncident.MaxPerTile = 1;
+            terrainFeatureIncident.SpawnChance = 2000;
+            terrainFeatureIncident.IsRepeating = true;
+            terrainFeatureIncident.MaxDelay = 5;
+            terrainFeatureIncident.MinDelay = 0;
+            terrainFeatureIncident.CanLaunchOutsideTile = true;
+            terrainFeatureIncident.Serialize = false;
+            terrainFeatureIncident.RequiresHumanInTile = false;
+            terrainFeatureIncident.RunOnTileEnter = false;
+            terrainFeatureIncident.ContainStructures = true;
+
+            return terrainFeatureIncident;
+        }
+
+
+    }
+    /*
+    [HarmonyPatch(typeof(TerrainFeatureSettings))]
+    [HarmonyPatch("Read")]
+    public class IncidentsPatch
+    {
+        static List<string> incidentsName = new List<string>();
+
+        [UsedImplicitly]
+        [HarmonyPostfix]
+        public static void TakePlantDrinkPatch(RocketBinaryReader reader, ref TerrainFeatureSettings __instance)
+        {
+            EventsMod.Log("IncidentsPatch:: TakePlantDrinkPatch --> carregando Incidents", EventsMod.Logs.INFO);
+            IncidentsPatch incidentsPatch = new IncidentsPatch();
+            __instance.IncidentFeatures.AddRange(incidentsPatch.addIncidents(incidentsName));
+        }
+
+
+        public IEnumerable<TerrainFeatureIncident> addIncidents(List<string> incidentsName)
+        {
+            List<TerrainFeatureIncident> response = new List<TerrainFeatureIncident>();
+            foreach (string incident in incidentsName)
+            {
+                TerrainFeatureIncident terrainFeatureIncident = new TerrainFeatureIncident();
+
+                response.Add((TerrainFeatureIncident)GetType().GetMethod(incident).Invoke(this, new object[] { terrainFeatureIncident }));
+
+            }
+            return response;
+        }
+
+        public void AddIncident(TerrainFeatureIncident terrainFeatureIncident)
+        {
+            EventsMod.Log("IncidentsPatch:: TakePlantDrinkPatch --> carregou Incidents " + 44, EventsMod.Logs.INFO);
+            terrainFeatureIncident.Type = 44;
+            terrainFeatureIncident.MaxPerTile = 1;
+            terrainFeatureIncident.SpawnChance = 2000;
+            terrainFeatureIncident.IsRepeating = true;
+            terrainFeatureIncident.MaxDelay = 5;
+            terrainFeatureIncident.MinDelay = 0;
+            terrainFeatureIncident.CanLaunchOutsideTile = true;
+            terrainFeatureIncident.Serialize = false;
+            terrainFeatureIncident.RequiresHumanInTile = false;
+            terrainFeatureIncident.RunOnTileEnter = false;
+            terrainFeatureIncident.ContainStructures = true;
+        }
+    }*/
+}
