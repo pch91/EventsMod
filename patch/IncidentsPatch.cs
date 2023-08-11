42f3015e1ee3bb2550795e3b7fa91903d266e29e
PCH91 <scorpionsk8er@hotmail.com>
2023-08-11 19:00:15 -0300

    fixs, logs and more Largest big fix, improviment fix, log fix, now can load events from mod.

--- patch/IncidentsPatch.cs
+++ patch/IncidentsPatch.cs
@@ -1,15 +1,16 @@
 ﻿using Assets.Scripts;
 using Assets.Scripts.Networking;
-using Assets.Scripts.Objects.Pipes;
 using BepInEx.Configuration;
+using BepInEx.Logging;
 using HarmonyLib;
 using JetBrains.Annotations;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using UnityEngine;
-using static Assets.Scripts.Networking.NetworkUpdateType.Thing.LogicUnit;
+using static System.Net.Mime.MediaTypeNames;
 using static WorldManager;
 
 namespace EventsMod.patch
@@ -19,35 +20,42 @@ namespace EventsMod.patch
     public class SetWorldEnvironmentsPatch
     {
         static List<string> incidentsName = new List<string>();
-        String chanincparam = (EventsMod.fconfigEvents["chanincparam"] as ConfigEntry<string>).Value;
-        String incMetName = (EventsMod.fconfigEvents["incMetName"] as ConfigEntry<string>).Value;
-        String confcinc = (EventsMod.fconfigEvents["confcinc"] as ConfigEntry<string>).Value;
 
         public SetWorldEnvironmentsPatch()
         {
-            addMethodNames();
+            if (!String.IsNullOrEmpty(StaticAttributes.incMetName))
+            {
+                addMethodNames();
+            }
         }
 
         [UsedImplicitly]
         [HarmonyPrefix]
         public static void prefixPatch(ref TerrainFeatureSettings __instance)
         {
-            if (bool.Parse(EventsMod.fmainconfig["EnabledMod"]?.ToString())) {
-                EventsMod.log("SetWorldEnvironmentsPatch:: prefixPatch --> carregando Incidents", EventsMod.Logs.DEBUG);
-                SetWorldEnvironmentsPatch incidentsPatch = new SetWorldEnvironmentsPatch();
-                EventsMod.log("SetWorldEnvironmentsPatch:: prefixPatch --> get incidentsName size " + incidentsName.Count(), EventsMod.Logs.DEBUG);
-                IEnumerable<TerrainFeatureIncident> lt = incidentsPatch.addIncidents(incidentsName);
-                EventsMod.log("SetWorldEnvironmentsPatch:: prefixPatch --> carregando " + lt.Count() + " Incidents", EventsMod.Logs.INFO);
-                foreach (TerrainFeatureIncident incident in lt)
+            try
+            {
+                if (bool.Parse(EventsMod.fmainconfig["EnabledMod"]?.ToString()))
                 {
-                    if (!TileSystem.Instance.CurrentIncidentTypes.ContainsKey(incident.Type))
+                    EventsMod.log("SetWorldEnvironmentsPatch:: prefixPatch --> carregando Incidents", EventsMod.Logs.DEBUG);
+                    SetWorldEnvironmentsPatch incidentsPatch = new SetWorldEnvironmentsPatch();
+                    EventsMod.log("SetWorldEnvironmentsPatch:: prefixPatch --> get incidentsName size " + incidentsName.Count(), EventsMod.Logs.DEBUG);
+                    IEnumerable<TerrainFeatureIncident> lt = incidentsPatch.addIncidents(incidentsName);
+                    EventsMod.log("SetWorldEnvironmentsPatch:: prefixPatch --> carregando " + lt.Count() + " Incidents", EventsMod.Logs.INFO);
+                    foreach (TerrainFeatureIncident incident in lt)
                     {
-                        TileSystem.Instance.CurrentIncidentTypes.Add(incident.Type, incident);
-                        TileSystem.WorldContainsIncidents = true;
-                        EventsMod.log("SetWorldEnvironmentsPatch:: prefixPatch --> adicionado Incident " + incident.Type, EventsMod.Logs.DEBUG);
+                        if (!TileSystem.Instance.CurrentIncidentTypes.ContainsKey(incident.Type))
+                        {
+                            TileSystem.Instance.CurrentIncidentTypes.Add(incident.Type, incident);
+                            TileSystem.WorldContainsIncidents = true;
+                            EventsMod.log("SetWorldEnvironmentsPatch:: TakePlantDrinkPatch --> adicionado Incident " + incident.Type, EventsMod.Logs.DEBUG);
+                        }
                     }
+                    EventsMod.log("SetWorldEnvironmentsPatch:: prefixPatch --> carregado", EventsMod.Logs.INFO);
                 }
-                EventsMod.log("SetWorldEnvironmentsPatch:: prefixPatch --> carregado", EventsMod.Logs.INFO);
+            }catch (Exception ex)
+            {
+                Debug.LogException(ex);
             }
         }
 
@@ -55,73 +63,61 @@ namespace EventsMod.patch
         {
             EventsMod.log("SetWorldEnvironmentsPatch:: addIncidents --> carregando Incidents", EventsMod.Logs.DEBUG);
             List<TerrainFeatureIncident> response = new List<TerrainFeatureIncident>();
-            if (!String.IsNullOrEmpty(confcinc))
-            {
-                response.AddRange(confiIncidents());
-            }
             foreach (string incident in incidentsName)
             {
-                EventsMod.log("SetWorldEnvironmentsPatch:: addIncidents --> Incident "+ incident, EventsMod.Logs.DEBUG);
+                EventsMod.log("SetWorldEnvironmentsPatch:: addIncidents --> Incident " + incident, EventsMod.Logs.DEBUG);
                 TerrainFeatureIncident terrainFeatureIncident = new TerrainFeatureIncident();
 
                 try
                 {
                     response.Add((TerrainFeatureIncident)GetType().GetMethod(incident, BindingFlags.NonPublic | BindingFlags.Instance).Invoke(this, new object[] { terrainFeatureIncident }));
-                }catch (Exception ex)
+                }
+                catch (Exception ex)
                 {
                     Debug.LogException(ex);
                 }
                 EventsMod.log("SetWorldEnvironmentsPatch:: addIncidents --> adicionado " + incident, EventsMod.Logs.DEBUG);
             }
+            if (!String.IsNullOrEmpty(StaticAttributes.confcinc))
+            {
+                response.AddRange(confiIncidents());
+            }
             return response;
         }
 
         // -------------------------------incidents-------------------------------------------------------
+
         private IEnumerable<TerrainFeatureIncident> confiIncidents()
         {
             EventsMod.log("SetWorldEnvironmentsPatch:: confiIncidents --> adicionando config incidents ", EventsMod.Logs.DEBUG);
             List<TerrainFeatureIncident> response = new List<TerrainFeatureIncident>();
-            List<String> configEvent = confcinc.Split('#').ToList();
+            List<String> configEvent = StaticAttributes.confcinc.Split('#').ToList();
             foreach (var item in configEvent)
             {
                 TerrainFeatureIncident terrainFeatureIncident = new TerrainFeatureIncident();
                 List<String> values = item.Split('|').ToList();
                 EventsMod.log("SetWorldEnvironmentsPatch:: confiIncidents --> adicionando value: " + item, EventsMod.Logs.DEBUG);
                 EventsMod.log("SetWorldEnvironmentsPatch:: confiIncidents --> adicionando type: " + values[0], EventsMod.Logs.DEBUG);
-                terrainFeatureIncident.Type = int.Parse(values[0]);
-                terrainFeatureIncident.MaxPerTile = int.Parse(values[1]);
-                terrainFeatureIncident.SpawnChance = int.Parse(values[2]);
-                terrainFeatureIncident.IsRepeating = bool.Parse(values[3]);
-                terrainFeatureIncident.MaxDelay = int.Parse(values[4]);
-                terrainFeatureIncident.MinDelay = int.Parse(values[5]);
-                terrainFeatureIncident.CanLaunchOutsideTile = bool.Parse(values[6]);
-                terrainFeatureIncident.Serialize = bool.Parse(values[7]);
-                terrainFeatureIncident.RequiresHumanInTile = bool.Parse(values[8]);
-                terrainFeatureIncident.RunOnTileEnter = bool.Parse(values[9]);
-                terrainFeatureIncident.ContainStructures = bool.Parse(values[10]);
+                terrainFeatureIncident.Type = Int32.Parse(values[0].Trim());
+                terrainFeatureIncident.MaxPerTile = Int32.Parse(values[1].Trim());
+                terrainFeatureIncident.SpawnChance = Int32.Parse(values[2].Trim());
+                terrainFeatureIncident.IsRepeating = Boolean.Parse(values[3].Trim());
+                terrainFeatureIncident.MaxDelay = Int32.Parse(values[4].Trim());
+                terrainFeatureIncident.MinDelay = Int32.Parse(values[5].Trim());
+                terrainFeatureIncident.CanLaunchOutsideTile = Boolean.Parse(values[6].Trim());
+                terrainFeatureIncident.Serialize = Boolean.Parse(values[7].Trim());
+                terrainFeatureIncident.RequiresHumanInTile = Boolean.Parse(values[8].Trim());
+                terrainFeatureIncident.RunOnTileEnter = Boolean.Parse(values[9].Trim());
+                terrainFeatureIncident.ContainStructures = Boolean.Parse(values[10].Trim());
                 response.Add(terrainFeatureIncident);
                 EventsMod.log("SetWorldEnvironmentsPatch:: confiIncidents --> adicionado", EventsMod.Logs.DEBUG);
             }
             return response;
         }
 
-        private void addMethodNames()
-        {
-            EventsMod.log("SetWorldEnvironmentsPatch:: addMethodNames --> adicionando incidents names", EventsMod.Logs.DEBUG);
-            if (!String.IsNullOrEmpty(incMetName)) {
-                List<String> configEvent = incMetName.Split('#').ToList();
-                foreach (var item in configEvent)
-                {
-                    EventsMod.log("SetWorldEnvironmentsPatch:: addMethodNames --> adicionando " + item, EventsMod.Logs.DEBUG);
-                    incidentsName.Add(item);
-                    EventsMod.log("SetWorldEnvironmentsPatch:: addMethodNames --> adicionado", EventsMod.Logs.DEBUG);
-                }
-            }
-        }
-
         private TerrainFeatureIncident testIncident(TerrainFeatureIncident terrainFeatureIncident)
         {
-            terrainFeatureIncident = AddIncident(44, 1, 2000, true, 5, 0, true, false, false, false, true,
+            terrainFeatureIncident = AddIncident(97, 1, 2000, true, 5, 0, true, false, false, false, true,
                 terrainFeatureIncident, System.Reflection.MethodBase.GetCurrentMethod().Name);
             return terrainFeatureIncident;
         }
@@ -129,7 +125,8 @@ namespace EventsMod.patch
 
         private TerrainFeatureIncident AddIncident(int type, int maxPerTile, int spawnChance, bool isRepeating,
             int maxDelay, int minDelay, bool canLaunchOutsideTile, bool serialize, bool requiresHumanInTile,
-            bool runOnTileEnter, bool containStructures, TerrainFeatureIncident terrainFeatureIncident, String methodname = null){
+            bool runOnTileEnter, bool containStructures, TerrainFeatureIncident terrainFeatureIncident, String methodname = null)
+        {
 
             terrainFeatureIncident.Type = type;
             terrainFeatureIncident.MaxPerTile = maxPerTile;
@@ -143,9 +140,9 @@ namespace EventsMod.patch
             terrainFeatureIncident.RunOnTileEnter = runOnTileEnter;
             terrainFeatureIncident.ContainStructures = containStructures;
 
-            if (methodname != null && !String.IsNullOrEmpty(chanincparam))
+            if (methodname != null && !String.IsNullOrEmpty(StaticAttributes.chanincparam))
             {
-                List<String> configEvent = chanincparam.Split('#').ToList();
+                List<String> configEvent = StaticAttributes.chanincparam.Split('#').ToList();
 
                 foreach (var item in configEvent)
                 {
@@ -154,7 +151,7 @@ namespace EventsMod.patch
                     //nameMethod | Param | value
                     if (methodname.Equals(values[0]))
                     {
-                        EventsMod.log("SetWorldEnvironmentsPatch:: AddIncident --> Alterando  Incidents " + values[0] +" -> "+ values[1], EventsMod.Logs.INFO);
+                        EventsMod.log("SetWorldEnvironmentsPatch:: AddIncident --> Alterando  Incidents " + values[0] + " -> " + values[1], EventsMod.Logs.INFO);
 
                         terrainFeatureIncident.GetType().GetField(values[1]).SetValue(terrainFeatureIncident, values[2]);
                     }
@@ -165,20 +162,51 @@ namespace EventsMod.patch
 
             return terrainFeatureIncident;
         }
-    }
+        private void addMethodNames()
+        {
+            EventsMod.log("SetWorldEnvironmentsPatch:: addMethodNames --> adicionando incidents names", EventsMod.Logs.DEBUG);
+            List<String> configEvent = StaticAttributes.incMetName.Split('#').ToList();
+            foreach (var item in configEvent)
+            {
+                EventsMod.log("SetWorldEnvironmentsPatch:: addMethodNames --> adicionando " + item, EventsMod.Logs.DEBUG);
+                incidentsName.Add(item);
+                EventsMod.log("SetWorldEnvironmentsPatch:: addMethodNames --> adicionado", EventsMod.Logs.DEBUG);
+            }
+        }
 
-    /*[HarmonyPatch(typeof(Incident))]
+
+    }
+    
+    [HarmonyPatch(typeof(Incident))]
     public class IncidentsLog3
     {
         [UsedImplicitly]
         [HarmonyPrefix]
-        [HarmonyPatch(typeof(Incident), "LoadIncident")]
-        public static void AddIncidentLog3(ref MissionData incidentData)
+        [HarmonyPatch(typeof(Incident), "GetIncident")]
+        public static void AddIncidentLog3(DirectoryInfo folder, object[] __args)
         {
-            if (incidentData != null) {
-                EventsMod.log("IncidentsLog3:: AddIncidentLog3 --> Incident " + incidentData.MissionName + " Type: " + incidentData.Type, EventsMod.Logs.INFO);
+            if (folder.FullName.Contains("EventsModGameData")) {
+                EventsMod.log("IncidentsLog3:: AddIncidentLog3 --> Load Incident: " + __args[0].ToString() , EventsMod.Logs.DEBUG);
             }
         }
-    }*/
+    }
 
+    [HarmonyPatch(typeof(ModData))]
+    public class ChangeGameDatafolder
+    {
+        [UsedImplicitly]
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(ModData), "GameDataFolder")]
+        public static void ChangeGameDatafolderPath(ref DirectoryInfo __result)
+        {
+            if (bool.Parse(EventsMod.fmainconfig["EnabledMod"]?.ToString()))
+            {
+                if (__result.FullName.Contains("\\Stationeers\\mods\\EventsMod\\GameData"))
+                {
+                    EventsMod.log("ChangeGameDatafolder:: ChangeGameDatafolderPath --> Original Path: " + __result.FullName, EventsMod.Logs.DEBUG);
+                    __result = new DirectoryInfo(__result.FullName.Replace("GameData", "EventsModGameData"));
+                }
+            }
+        }
+    }
 }
